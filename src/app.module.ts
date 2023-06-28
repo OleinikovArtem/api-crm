9fa994a335fa60f62994c6f83546127762f2f957
Artem Oleinikov <artem.oleinikov.web@gmail.com>
2023-06-28 15:49:59 +0300

    Updated structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,16 @@ import { ProductsModule } from './modules/products/products.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { PrismaModule } from './database/prisma.module';
-import { ApiModule } from './api/api.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
-    }), PrismaModule, ApiModule, ProductsModule],
+    }),
+    PrismaModule,
+    ProductsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductsRepository } from './products.repository';
 import { PrismaModule } from 'src/database/prisma.module';
 import { ProductsService } from './products.service';
+import { ProductResolver } from './products.resolver';
 
 @Module({
   imports: [PrismaModule],
-  providers: [ProductsRepository, ProductsService],
+  providers: [ProductsRepository, ProductsService, ProductResolver],
   exports: [ProductsService]
 })
 export class ProductsModule {}
